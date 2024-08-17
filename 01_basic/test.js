8b04b1cba1ea75ef5b5fa85140728dfd7d15a70b
vivek-jangela <vivekjangela@gmail.com>
2024-08-17 20:19:19 +0000

    datatype

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,25 @@
-console.log("vivek")
+// alert(3 +3)// we are using nodejs, not browser
+console.log(3
+    +
+    3)//code redability should be high
+    //console.log("hitesh")
+
+    // datatype
+
+    // premetive data type
+    let name ="vivek"
+    let age =18
+    let isLoggedIn = false
+    // no. => 2 to power 53
+    // bigint
+    // string = ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol =>unique 
+
+
+    // object
+    console.log(typeof undefined); // undefiend type
+    console.log( typeof null); //object
+    
