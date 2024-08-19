857cfad01cd85cfe2aab7ed7a6aa668d47bcccf4
vivek-jangela <vivekjangela@gmail.com>
2024-08-19 18:14:38 +0000

    conversion of data type

---
+++ 01_basic/02test.js
@@ -0,0 +1,36 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//let score = "33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+// "3abc" => NaN
+// true => 1; false =>0
+
+
+//boolen
+
+//let isLoggedIn = "vivek"
+//let booleanIsLogedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLogedIn)
+
+
+//1 =>true; //0=>false;
+// "" =>false; // "vivek" => true;
+
+
+
+
+//STRINGS
+
+let string = 33
+let unstring = String(string)
+console.log(unstring)
+console.log(typeof unstring)
