36e8564d60773b80cc5c31f2fbd4f26472f004b1
vivek-jangela <vivekjangela@gmail.com>
2024-08-21 08:38:18 +0000

    primitive and refrence

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+//7 type : Boolearn, String, Number, null, undefined, Symbol,BigInt
+
+// const score =100;
+//  const scoreValue = 100.3;
+
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail;
+
+// const id = Symbol('123')
+ const anotherId = Symbol ('123')
+// console.log(id === anotherId);
+
+// const bigNumber = 3342344523443n
+
+//Refrence (Non - primitive)
+
+//Array, Object , Function
+//DATATYPE OF REFRENCE ALWAYES BE A FUNCTION
+//BUT SPECIALY FUCTION DATATYPE BE A OBJECT FUNCTION
+
+const heros =["viveek", "adi"] ;
+let myObj = {
+    name : "hitesh",
+    age : 22,
+}
+const myFunct = function(){
+    console.log("hitesh world")
+}
+console.log(typeof anotherId);
+
+//Q1)js is dynamic or static lang?
+//Ans 1)js is a dynamicly type lang.
