a737d547148897ce9cde5577ce89b320ca5ab64b
vivek-jangela <vivekjangela@gmail.com>
2024-08-24 19:22:56 +0000

    math_num

---
+++ 01_basic/05num_and_math.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score)
+const balance = new  Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 112.89
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ***********Maths********************
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
