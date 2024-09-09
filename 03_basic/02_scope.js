064474eb8e7a72e9ac309f59eb5fc820296a4bcf
vivek-jangela <vivekjangela@gmail.com>
2024-09-09 18:16:38 +0000

    scope2

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -1,53 +1,53 @@
-// var c = 300
-let a= 300
-if (true) {
-    let a= 10;
-    const b=20
-    console.log("inner;", a);
+// // var c = 300
+// let a= 300
+// if (true) {
+//     let a= 10;
+//     const b=20
+//     // console.log("inner;", a);
     
 
-}
-// console.log(a);
+// }
+// // console.log(a);
 
-function one() {
-    const username = "hitesh"
-    function two() {
-        const website = "youtube"
-        console.log(username);
+// function one() {
+//     const username = "hitesh"
+//     function two() {
+//         const website = "youtube"
+//         // console.log(username);
         
         
-    }
-    // console.log(website);
-    // two()
+//     }
+//     // console.log(website);
+//     // two()
     
     
-}
-// one()
-// console.log(username);
+// }
+// // one()
+// // console.log(username);
 
-if (true) {
-    const username = "vivek"
-    if (username==="vivek") {
-        const website ="youtube"
-        console.log(username + website);
+// if (true) {
+//     const username = "vivek"
+//     if (username==="vivek") {
+//         const website ="youtube"
+//         // console.log(username + website);
         
         
-    }
-    // console.log(website);
+//     }
+//     // console.log(website);
     
-}
+// }
 // console.log(username);
 
 
 // +++++++++++++++++++interesting+++++++++++++++++++++++++++++
-
+console.log(addone(5))
 function addone(num) {
     return num + 1
     
 }
-addone(5)
-function addtwo(num) {
+
+const addtwo = function(num) {
     return num + 1
     
 }
-addone(5)
+addtwo(5)
