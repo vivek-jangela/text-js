1c473bf9024751d76f7754aa217647de39fdd06b
vivek-jangela <vivekjangela@gmail.com>
2024-09-10 19:25:02 +0000

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,45 @@
+//for
+
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    if (element ==5) {
+        // console.log("5 is best");
+    }
+    // console.log(element);
+    
+    
+}
+ // console.log(element);
+
+ for (let i = 1; i <=10; i++) {
+    // console.log(`outer loop value : ${i}`); 
+    for (let j = 1; j <=10; j++) {
+        // console.log(i + '*' + j +'=' +i*j);      
+    }
+ }
+let myarray = ["flash", "superman","batman"]
+// console.log(myarray.length);
+
+for (let index = 0; index <myarray.length; index++) {
+    const element = myarray[index];
+    // console.log(element);
+    
+}
+// for (let index = 1; index <=20; index++) {
+//     if (index == 5) {
+//         console.log('detcted 5');
+//         break
+        
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+for (let index = 1; index <=20; index++) {
+    if (index == 5) {
+        console.log('detcted 5');
+        
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+}
