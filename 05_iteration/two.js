cd2b922fc02222dcdf4e050ab88bcab75f5aa415
vivek-jangela <vivekjangela@gmail.com>
2024-09-11 10:18:54 +0000

    two

---
+++ 05_iteration/two.js
@@ -0,0 +1,21 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["superman", "batman","hipo"]
+let arr = 0
+while (arr<myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+
+// -------------Do while loop
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10);
