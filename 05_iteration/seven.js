991c351469fcc725b01cb9aad7422e99972b797b
vivek-jangela <vivekjangela@gmail.com>
2024-09-11 19:33:56 +0000

    seven2

--- 05_iteration/seven.js
+++ 05_iteration/seven.js
@@ -10,3 +10,9 @@
 // )
 // console.log(value);
 
+const mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newnum = mynum
+          .map( (num) => num+10)
+          .map( (num) => num-10)
+          .filter( (num) => num>=4)
+console.log(newnum);
