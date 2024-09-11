7313c2207f8e150ec2d6f09911a33faabd5e0bfc
vivek-jangela <vivekjangela@gmail.com>
2024-09-11 13:23:57 +0000

    seven

---
+++ 05_iteration/seven.js
@@ -0,0 +1,12 @@
+// const mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newnum = mynum.map( (num)=>{ return num + 10 })
+// console.log(newnum);
+
+// const mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const value = mynum.forEach((num)=>{
+//     console.log(num+10);
+//     return num
+// }
+// )
+// console.log(value);
+
